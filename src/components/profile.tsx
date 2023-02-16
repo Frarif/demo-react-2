0e0c0cdad4d00fd255473003b4c32bf8ed852e33
Frarif <freakinarif25@gmail.com>
2023-02-16 19:24:16 +0700

    add: background homepage

--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -1,21 +1,21 @@
 export function Navbar() {
   return (
-    <div className="h-20">
+    <section className="h-20">
       <nav className="flex justify-evenly content-center items-center">
         <a href="#">
           <img src="src/images/header-logo.png" alt="Header Logo" />
         </a>
         <ul className="flex gap-20">
-          <li className="font-bold text-lg text-green-800 hover:text-cyan-500 ">
+          <li className="font-bold text-base text-green-800 hover:text-cyan-500 ">
             HOME
           </li>
-          <li className="font-bold text-lg text-green-800 hover:text-cyan-500 ">
+          <li className="font-bold text-base text-green-800 hover:text-cyan-500 ">
             ABOUT US
           </li>
-          <li className="font-bold text-lg text-green-800 hover:text-cyan-500 ">
+          <li className="font-bold text-base text-green-800 hover:text-cyan-500 ">
             EXPLORE
           </li>
-          <li className="font-bold text-lg text-green-800 hover:text-cyan-500 ">
+          <li className="font-bold text-base text-green-800 hover:text-cyan-500 ">
             CONTACT US
           </li>
         </ul>
@@ -23,6 +23,6 @@ export function Navbar() {
           SIGN UP
         </button>
       </nav>
-    </div>
+    </section>
   );
 }
